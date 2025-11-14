10a47bb5123983a2d4291a21f2cb9aefa7fb8852
Shreyesh <shreyeshpachpor06@gmail.com>
2025-11-14 17:29:06 +0530

    vite and react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Ken from './Ken';
+
+function App() {
+  return (
+<>
+<Ken />
+
+</>// function lo and usme html return karo and index js usko render kardega
+
+  );
+}
+
+export default App;   
---
+++ 01basicreact/src/Ken.jsx
@@ -0,0 +1,8 @@
+function Ken(){
+    return(
+        <h2>This is ken page</h2>
+    )
+}
+
+
+export default Ken;
