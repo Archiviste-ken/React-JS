8dc7ca611f0dd5ab36420ff085c9cb0df6869627
Shreyesh <shreyeshpachpor06@gmail.com>
2025-11-15 19:19:51 +0530

    working of hooks

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react'
+import { useState } from 'react'  // hook yahi se aata hai
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-    useState
- 
-  let counter = 15
+
+    let [counter, setCounter] = useState(15) // setCounter is function to update counter value
+ // useState hook is used to create state variable in functional componentate 
+  // let counter = 15
 
   const addValue = () => {
     console.log("clicked", counter);
     
-   counter = counter + 1
+  //  counter = counter + 1
+  setCounter(counter + 1)  // updating the counter value using setCounter function
 }
+
+  const removeValue = () => {
+    console.log("remove clicked", counter);
+    
+    // counter = counter - 1
+    setCounter(counter - 1)  // updating the counter value using setCounter function
+  }
     
   return (
     <>
@@ -22,7 +31,8 @@ function App() {
       <button
       onClick= {addValue}>Add button {counter}</button>
       <br />
-      <button>remove value {counter} </button>
+      <button
+      onClick = {removeValue}>remove value {counter} </button>
       <p>footer: {counter}</p>
     </>
   )
