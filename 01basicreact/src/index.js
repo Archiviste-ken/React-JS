99857b1b965648bb2e44f2707eed19572318b17c
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-02 20:24:09 +0530

    fix: correct import statements and formatting in index.js

--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';  // core foundational library of react which takes the reference of react and convert it into html.
+// import React from 'react';  // core foundational library of react which takes the reference of react and convert it into html.
 
-import ReactDOM from 'react-dom/client';// this is implementation of  react in the web environment just like react native is for mobile environment.
+// import ReactDOM from 'react-dom/client';// this is implementation of  react in the web environment just like react native is for mobile environment.
 
-import App from './App';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
 
-    <App />  // now we are able to write html using js.
+//     <App />  // now we are able to write html using js.
 
 
 
     
-); 
+// ); 
+
+
+import React from ''react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+
+    <App />
+)
