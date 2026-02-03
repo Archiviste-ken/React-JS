0a84dd1f3f8c6e3a14bb627e63e03d7dd795b434
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-03 22:42:34 +0530

    clean up main.jsx by removing commented-out code and unnecessary whitespace

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+
     <App />
-  
 
 ) 
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(
+//     <App />
+// )
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <App />
+// )
