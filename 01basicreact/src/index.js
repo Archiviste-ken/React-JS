97df55d26f13f75e8c267ffb4f55efba1af9d850
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-03 14:48:20 +0530

    fix: update rendering method in index.js and comment out old implementation

--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -19,9 +19,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
+// root.render(
 
+//     <App />
+// )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
+    
 )
