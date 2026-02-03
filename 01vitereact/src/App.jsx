6364f53fc09a8ceb86d5a70cf8f72755625ea42c
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-03 16:10:19 +0530

    refactor App component to include additional content and improve structure

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -5,8 +5,14 @@ function App() {
 
 
   return (
-    <Chai />
+    <>
+         <Chai />
+         <h1 style={{color: 'blue'}}>planet caravan is divine.</h1>
+         <p>this is a paragraph tag</p>
+    </>
   );
+//  we can use <div></div> or <></> (fragment) as wrapper
 }
 
 export default App
+
