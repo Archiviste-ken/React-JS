150d4e7ad364c175d4b70a6af0fbb2e481d248f5
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-05 23:06:26 +0530

    fix: comment out unused imports and rendering code in main.jsx

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,20 +1,32 @@
-import App from './App.jsx'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <App />
-
-) 
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// root.render(
-//     <App />
-// )
+// import App from './App.jsx'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
+
 //     <App />
-// )
+
+// ) 
+
+
+// // const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// // root.render(
+// //     <App />
+// // )
+
+// // ReactDOM.createRoot(document.getElementById('root')).render(
+// //     <App />
+// // )
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '/App.jsx'
+
+
+function MyApp() {
+
+    return {
+        <div>
+        <h1> </h1></div>
+    }
