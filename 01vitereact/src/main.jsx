0997e5126748652739846a85dd5320fda1e34f29
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-06 16:50:19 +0530

    fix: update rendering logic to use React.createElement for link and comment out unused reactElement definition

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -44,20 +44,27 @@ function MyApp(){
     )
 }
 
-const reactElement = {
+// const reactElement = {
 
-    type: 'a',
-    props: {href: 'https://google.com',
-        target: '_blank'
-    },
-    children: 'Click me to visit google'
-    }
+//     type: 'a',
+//     props: {href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+//     }
 
 const anotherElement = (
     <a href="https://google.com" target='_blank'> Visit google</a>
 )
 
+const reactElement = React.createElement(
+
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'click me to visit google'
+
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-   anotherElement
+   reactElement
 )
