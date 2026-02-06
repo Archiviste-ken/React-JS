9b2d2d25c07bdecbeb92af4ea9834a44f303f228
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-06 16:32:09 +0530

    fix: update MyApp function to render a title and correct import path for App component

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -19,14 +19,45 @@
 // //     <App />
 // // )
 
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from '/App.jsx'
+
+
+// function MyApp() {
+
+//     return {
+//         <div>
+//         <h1> </h1></div>
+//     }
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '/App.jsx'
+import App from './App.jsx'
 
 
-function MyApp() {
-
-    return {
+function MyApp(){
+    return(
         <div>
-        <h1> </h1></div>
+            <h1>Planet Caravan!</h1>
+        </div>
+    )
+}
+
+const reactElement = {
+
+    type: 'a',
+    props: {href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
     }
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'> Visit google</a>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+   anotherElement
+)
