88dee442ffd3d474cfce714b068542ebfa0829cc
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-06 17:33:36 +0530

    fix: update App component to include username in the heading

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,12 +3,12 @@ import Chai from './chai'
 
 
 function App() {
-
+  const username = 'chai aur code'
 
   return (
     <>
          <Chai />
-         <h1 style={{color: 'blue'}}>planet caravan is divine.</h1>
+         <h1 style={{color: 'blue'}}>planet caravan is divine {username}</h1>
          <p>this is a paragraph tag</p>
     </>
   );
