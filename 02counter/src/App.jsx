6d8e5bdeefff704458069fd3fcc19bc464bb16dc
Shreyesh <shreyeshpachpor06@gmail.com>
2026-02-09 22:45:41 +0530

    use of prevCounter to avoid the effect of useState.

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -11,10 +11,10 @@ function App() {
 
   const addValue = () => {
     console.log("clicked",counter);
-    setCounter(counter + 1)
-    setCounter(counter + 1)
-    setCounter(counter + 1)
-    setCounter(counter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
   }
 
   const removeValue = () =>{
