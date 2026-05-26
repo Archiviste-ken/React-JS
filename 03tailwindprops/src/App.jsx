94f4da972d1ed04e4e3d366ec91ba232a0381758
Shreyesh <shreyeshpachpor06@gmail.com>
2026-05-26 14:45:25 +0530

    refactor: clean up App component and remove unused styles

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,25 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Card from './components/card'
+import { useState } from "react";
+import "./App.css";
+import Card from "./components/card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-let myObj = {
-  username: "atomic",
-  age : 25
-}
-let newArr = [1,2,3]
+  let myObj = {
+    username: "atomic",
+    age: 25,
+  };
+  let newArr = [1, 2, 3];
   return (
     <>
-   <h1 className='bg-green-400 text-black p-4 rounded-xl '> Tailwind Test</h1>
-
-<Card username = "ReactJs" btnText="Click Me"/>
-<Card username = "Kaneki" btnText="Follow" />
+      <main>
+        <div className="bg-black">
+          <h1 className="bg-green-400 text-black p-4 rounded-xl m-auto max-w-fit flex ">
+            Tailwind Tes
+          </h1>
+          <Card username="ReactJs" btnText="Click Me" />
+          <Card username="Kaneki" btnText="Follow" />
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 03tailwindprops/src/components/card.jsx
+++ 03tailwindprops/src/components/card.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
+import React from "react";
 
-function card({username, btnText = "visit me"}) {
-    console.log(username, btnText);
-    
+function card({ username, btnText = "visit me" }) {
+  console.log(username, btnText);
+
   return (
-    
-   <div className="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
-  <div>
-    <img className="size-48 shadow-xl rounded-md" alt="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNubLmqdOK9pZWU-2IiD20cuSIdUUDi9-NvQ&s" />
-  </div>
-  <div className="flex items-center md:items-start">
-    <span className="text-2xl font-medium">{username}</span>
-    <span className="font-medium text-sky-500">{username}</span>
-    <span className="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
-      <span>{btnText}</span>
-      <span>·</span>
-      <span>{btnText}</span>
-    </span>
-  </div>
-</div>
-  )
+    <div className="flex items-center">
+      <div  >
+        <img 
+          className="size-48 shadow-xl rounded-md"
+          alt=""
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNubLmqdOK9pZWU-2IiD20cuSIdUUDi9-NvQ&s"
+        />
+      </div>
+      <div className="flex items-center md:items-start">
+        <span className="text-2xl font-medium">{username}</span>
+        <span className="font-medium text-sky-500">{username}</span>
+        <span className="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+          <span>{btnText}</span>
+          <span>·</span>
+          <span>{btnText}</span>
+        </span>
+      </div>
+    </div>
+  );
 }
 
-export default card
-
+export default card;
 
 // props are the way to pass data from parent component to child component in react. It is a read only data, it cannot be modified by the child component. It is used to pass data from parent component to child component. It is also used to pass data from child component to parent component using callback functions.
